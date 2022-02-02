eb8baa8a98155468f5dc1a7972a729b5426560a4
Benjamin Benitez <benbenitezhez@gmail.com>
2022-02-01 20:29:09 -0500

    Defined function playRound to complte one full game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,23 @@
-//const hand = ["Rock", "Paper", "Scissors"];
-//function computerPlay() {
-    //return hand[Math.floor(Math.random() * hand.length)];
-//}
+const hand = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    return hand[Math.floor(Math.random() * hand.length)];
+}
+//console.log(computerPlay());
+
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == "Paper") || (playerSelection == "paper" && computerSelection == "Scissors") || (playerSelection == "scissors" && computerSelection == "Rock")) {
+        return alert("You Lose! " + computerSelection + " beats " + playerSelection);
+    } else if ((playerSelection == "paper" && computerSelection == "Rock") || (playerSelection == "scissors" && computerSelection == "Paper") || (playerSelection == "rock" && computerSelection == "Scissors")) {
+        return alert("You Win! " + playerSelection + " beats " + computerSelection);
+    } else (playerSelection == computerSelection)
+        return alert("Its a Tie!");
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+
+}
