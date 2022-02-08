62ae258fc458202c911c9c6abc98d5eb94102d5c
Benjamin Benitez <benbenitezhez@gmail.com>
2022-02-08 16:29:05 -0500

    Added prompt() function within playround() function. This allows the game() to invoke the playRound() 5 times and get different results.Did not finish working on the playSelection() function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,23 +1,57 @@
+//console.log(computerPlay()); The function returns a random choice between rock,paper, and scissorss
 const hand = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
     return hand[Math.floor(Math.random() * hand.length)];
 }
-//console.log(computerPlay());
+/*playSelection() prompts the user for their choice and generates the computer play
+function playSelection() {
+    let playerInput = prompt("Choose your hand?", "Rock, paper or scissors?").toLowerCase();
+    playerSelection = playerInput;
+    computerSelection = computerPlay();
+}
+//console.log(playSelection());
+*/
+
+let playerSelection;
+let computerSelection;
 
 function playRound(playerSelection, computerSelection) {
+    let playerInput = prompt("Choose your hand?", "Rock, paper or scissors?").toLowerCase();
+    playerSelection = playerInput;
+    computerSelection = computerPlay();
+
     if ((playerSelection == "rock" && computerSelection == "Paper") || (playerSelection == "paper" && computerSelection == "Scissors") || (playerSelection == "scissors" && computerSelection == "Rock")) {
-        return alert("You Lose! " + computerSelection + " beats " + playerSelection);
+        return ("You Lose! " + computerSelection + " beats " + playerSelection);
     } else if ((playerSelection == "paper" && computerSelection == "Rock") || (playerSelection == "scissors" && computerSelection == "Paper") || (playerSelection == "rock" && computerSelection == "Scissors")) {
-        return alert("You Win! " + playerSelection + " beats " + computerSelection);
+        return ("You Win! " + playerSelection + " beats " + computerSelection);
     } else (playerSelection == computerSelection)
-        return alert("Its a Tie!");
+        return ("Its a Tie!");
 }
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-
+/*
+playRound(playerSelection, computerSelection);
+console.log(playerSelection);
+console.log(computerSelection);
+*/
 function game() {
-
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
+    console.log(playRound(playerSelection, computerSelection));
 }
+
+//let playerScore = 0;
+//let computerScore = 0;
+
+function gameScore() {
+    if ((playerSelection == "rock" && computerSelection == "Paper") || (playerSelection == "paper" && computerSelection == "Scissors") || (playerSelection == "scissors" && computerSelection == "Rock")) {
+        computerScore++;
+        return ("The computer has " + computerScore + " points.");
+    } else if ((playerSelection == "paper" && computerSelection == "Rock") || (playerSelection == "scissors" && computerSelection == "Paper") || (playerSelection == "rock" && computerSelection == "Scissors")) {
+        playerScore++;
+        return ("You have " + playerScore + " points.");
+    } else (playerSelection == computerSelection)
+}
+
+//game();
