f7c028dede87120e3b16af75cc3fbdb5ff44060f
Benjamin Benitez <benbenitezhez@gmail.com>
2022-02-10 20:25:28 -0500

    Completed the RPS project. Implemented two new functions, to check score and reset score onece function is complete.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,9 +12,11 @@ function playSelection() {
 }
 //console.log(playSelection());
 */
-
+//created global variables. Is there a way to create scope variables?
 let playerSelection;
 let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     let playerInput = prompt("Choose your hand?", "Rock, paper or scissors?").toLowerCase();
@@ -22,28 +24,46 @@ function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
 
     if ((playerSelection == "rock" && computerSelection == "Paper") || (playerSelection == "paper" && computerSelection == "Scissors") || (playerSelection == "scissors" && computerSelection == "Rock")) {
-        return ("You Lose! " + computerSelection + " beats " + playerSelection);
+        computerScore++;
+        return (computerSelection + " beats " + playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + ". The computer has: " + computerScore + " points. You have: " + playerScore + " points.");
     } else if ((playerSelection == "paper" && computerSelection == "Rock") || (playerSelection == "scissors" && computerSelection == "Paper") || (playerSelection == "rock" && computerSelection == "Scissors")) {
-        return ("You Win! " + playerSelection + " beats " + computerSelection);
+        playerScore++;
+        return (playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1) + " beats " + computerSelection + ". The computer has: " + computerScore + " points. You have: " + playerScore + " points.");
     } else (playerSelection == computerSelection)
-        return ("Its a Tie!");
+        return ("Tie!" + " The computer has: " + computerScore + " points. You have: " + playerScore + " points.");
 }
 /*
 playRound(playerSelection, computerSelection);
 console.log(playerSelection);
 console.log(computerSelection);
 */
+
+function scoreCheck() {
+    if (computerScore > playerScore) {
+        return ("The computer wins the game!");
+    } else if (playerScore > computerScore) {
+        return ("You win the game!")
+    } else (playerScore == computerScore)
+    return ("Tie");
+}
+
+function scoreReset() {
+    computerScore = 0;
+    playerScore = 0;
+}
+
+
 function game() {
     console.log(playRound(playerSelection, computerSelection));
     console.log(playRound(playerSelection, computerSelection));
     console.log(playRound(playerSelection, computerSelection));
     console.log(playRound(playerSelection, computerSelection));
     console.log(playRound(playerSelection, computerSelection));
+    console.log(scoreCheck());
+    scoreReset();
 }
 
-//let playerScore = 0;
-//let computerScore = 0;
-
+/*
 function gameScore() {
     if ((playerSelection == "rock" && computerSelection == "Paper") || (playerSelection == "paper" && computerSelection == "Scissors") || (playerSelection == "scissors" && computerSelection == "Rock")) {
         computerScore++;
@@ -53,5 +73,6 @@ function gameScore() {
         return ("You have " + playerScore + " points.");
     } else (playerSelection == computerSelection)
 }
+*/
 
 //game();
